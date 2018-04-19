a01fc074dfdbee09f7f0458fc6bcf1a42258d74b
wals38 <wals.38100@gmail.com>
2018-04-19 10:05:48 +0200

    Ajout cours perl.developpez.com [2/3]

---
+++ PDPerl-02.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+
+sub fonction0 {
+	print "Appel de fonction effectué !\n";
+}
+
+sub inverse {										# L'argument est implicite, avec la variable $_
+	if ($_[0] == 0) {
+		print "Division par 0 impossible !\n";
+	} else {
+		print "Linverse de " . $_[0] . " est " . inverse2($_[0]) . ".\n";
+	}
+}
+
+sub inverse2 {
+	return 1/$_[0] unless ($_[0] == 0);
+}
+
+print "Début...\n";
+fonction0();
+
+print "\nBien, de quel nombre voulez vous maintenant avoir l'inverse : ";
+my $nb = <STDIN>;
+chomp($nb);
+
+inverse($nb);
+
+print "\nFin.";
--- PDPerl.pl
+++ PDPerl.pl
@@ -15,7 +15,6 @@ unless($nb == 0) {
 my @table = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 print "\nAffichons maintenant sa table de multiplication :\n";
-
 foreach my $i (@table) {
 	if ($i < 10) {
 		print "\t" . $nb . " x " . $i . "  = " . $nb*$i . "\n";
@@ -25,8 +24,7 @@ foreach my $i (@table) {
 }
 
 print "\nEncore, juste pour le fun :\n";
-
-foreach (@table) {													# C'est la variable par defaut $_ qui est utilisée ici
+foreach (@table) {								# C'est la variable par defaut $_ qui est utilisée ici
 	if ($_ < 10) {
 		print "\t" . $nb . " x " . $_ . "  = " . $nb*$_ . "\n";
 	} else {
@@ -34,3 +32,25 @@ foreach (@table) {													# C'est la variable par defaut $_ qui est utilis�
 	}
 }
 
+print "\nEntrez un nom de fichier : ";
+my $file = <STDIN>;
+chomp($file);
+
+if (-f $file) {									# Cette ligne verifie si $file existe dans le repertoire en cours
+	print $file . " existe.";
+} else {
+	print $!;
+}
+
+print "\n\nListe de chez paramètres passés au programme : \n";
+foreach (@ARGV) {
+	print $_ . "\n";
+}
+
+print "\nVoici quelques informations utiles sur votre machine : ";
+
+while ((my $cle, my $valeur) = each (%ENV)) {
+	print $cle . " a comme valeur " . $valeur . ".\n";
+}
+
+print "\nVotre processeur est le suivant : " . $ENV{'PROCESSOR_IDENTIFIER'} . ".\n\n\nFin.\n";
